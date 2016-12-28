6deec7d154110b11ad43becb3d5a7117456344cb
Mikhail <miksolodovnikov@gmail.com>
2016-12-28 19:44:12 +0500

    Add function to draw the points on the which generates with the Monte Carlo simulation.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import random
 import time
 import logging
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
@@ -19,8 +20,27 @@ def estimate_pi(trials):
     return 4 * count / trials
 
 
+def generate_sample(trials):
+    in_x, in_y, out_x, out_y = [], [], [], []
+    for _ in range(trials + 1):
+        x, y = random(), random()
+        if x * x + y * y <= 1:
+            in_x.append(x), in_y.append(y)
+        else:
+            out_x.append(x), out_y.append(y)
+    return in_x, in_y, out_x, out_y
+
+
+def draw_estimation(trials):
+    in_x, in_y, out_x, out_y = generate_sample(trials)
+    plt.plot(in_x, in_y, 'x', color='r')
+    plt.plot(out_x, out_y, 'o', color='b')
+    plt.show()
+
+
 def main():
     print(estimate_pi(10000000))
+    draw_estimation(10000)
 
 
 if __name__ == '__main__':
